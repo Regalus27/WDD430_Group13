406c87a8dda7785c30d71330210a22fd07ea2dad
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2025-01-25 16:35:01 -0600

    Initial Layout Concept

--- group-13-project/src/app/page.tsx
+++ group-13-project/src/app/page.tsx
@@ -1,95 +1,121 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <div className="container mx-auto">
+        <div className="flex flex-row flex-wrap py-4">
+          <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
+            <div className="sticky top-0 p-4 w-full">
+              {/** Navigation / Search */}
+              <ul className="flex flex-col overflow-hidden">
+                <p>Search</p>
+                <p className="underline">Landing Page</p>
+                <p>Other Navigation</p>
+                <p>Mockup</p>
+              </ul>
+            </div>
+          </aside>
+          <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
+            {/** Featured Cards */}
+            <div className="grid grid-cols-3 gap-2">
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+            </div>
+            {/** Newest Cards */}
+            <div className="grid grid-cols-4 gap-2">
+            <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+
+              <a className="group black relative overflow-hidden rounded-lg" href="#">
+                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image">
+                  {/** Price Here */}
+                </img>
+              </a>
+            </div>
+          </main>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </div>
+      {/** Footer */}
+      <footer className="mt-auto">
+          <h1>Footer</h1>
       </footer>
-    </div>
+    </>
   );
 }
