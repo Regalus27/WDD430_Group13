3924c6ec7d284190a27ecd3865e8194b4da57d8b
Jacob Perry <perryjay123@live.com>
2025-01-29 11:30:06 -0800

    Config Tailwind, UI Cleanup

--- group-13-project/src/app/page.tsx
+++ group-13-project/src/app/page.tsx
@@ -1,89 +1,32 @@
 import FeaturedCard from "./featuredCard";
+import { ulid } from "ulid";
 
 export default function Home() {
+
+  const arr = new Array(24).fill(1).map((v,i) => ulid());
+  console.log(arr)
+  
   return (
-    <>
-      <h1 className="text-center">Handcraft Haven</h1>
-      <div className="container mx-auto">
-        <div className="flex flex-row flex-wrap py-4">
-          <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
-            <div className="sticky top-0 p-4 w-full">
-              {/** Navigation / Search */}
-              <ul className="flex flex-col overflow-hidden">
-                <p>Search</p>
-                <p>Landing Page</p>
-                <p>Other Navigation</p>
-                <p>Mockup</p>
-              </ul>
-            </div>
-          </aside>
-          <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
-            {/** Featured Cards */}
-            <h1>Featured Items</h1>
-            <div className="grid grid-cols-3 gap-2">
-              {/** TODO: Pull information from database and feature 3 algorithmically (probably random) items. */}
-              <FeaturedCard itemPicture={"mockup.png"} itemAltText={"Mockup Item Image"} itemPrice={1599} itemPageLink={"#"} />
-              <FeaturedCard itemPicture={"mockup.png"} itemAltText={"Mockup Item Image"} itemPrice={399} itemPageLink={"#"} />
-              <FeaturedCard itemPicture={"mockup.png"} itemAltText={"Mockup Item Image"} itemPrice={2000} itemPageLink={"#"} />
-            </div>
-            {/** Newest Cards */}
-            <h1>Newest Items</h1>
-            <div className="grid grid-cols-4 gap-2">
-            <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-
-              <a className="group black relative overflow-hidden rounded-lg" href="#">
-                <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
-              </a>
-            </div>
-          </main>
-        </div>
+    <main role="main" className="w-full">
+      {/** Featured Cards */}
+      <h1>Featured Items</h1>
+      <div className="grid grid-cols-3 gap-2">
+        {/** TODO: Pull information from database and feature 3 algorithmically (probably random) items. */}
+        <FeaturedCard itemPicture={"mockup.png"} itemAltText={"Mockup Item Image"} itemPrice={1599} itemPageLink={"#"} />
+        <FeaturedCard itemPicture={"mockup.png"} itemAltText={"Mockup Item Image"} itemPrice={399} itemPageLink={"#"} />
+        <FeaturedCard itemPicture={"mockup.png"} itemAltText={"Mockup Item Image"} itemPrice={2000} itemPageLink={"#"} />
       </div>
-      {/** Footer */}
-      <footer className="mt-auto">
-          <h1 className="text-center">Footer</h1>
-      </footer>
-    </>
+      {/** Newest Cards */}
+      <h1>Newest Items</h1>
+      <div className="grid grid-cols-4 gap-2">
+        {arr.map((v) => {
+          return (
+            <a key={v} className="group black relative overflow-hidden rounded-lg" href={`/product/${v}`}>
+              <img className="w-full size-40 object-cover bg-gray-100 rounded-lg dark:bg-neutral-800" src="mockup.png" alt="Mockup Item Image"></img>
+            </a>
+          )
+        })}
+      </div>
+    </main>
   );
 }
---
+++ group-13-project/src/app/ui/footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => (
+  <footer className="bg-azure-950 mt-10 text-seafoam-100 flex flex-row justify-center min-h-[100px] items-center">
+      <p>&#169; 2025 | WDD 430 Group 13</p>
+  </footer>
+);
