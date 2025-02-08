fae5cc59ab4d2d2170ff8c3a3775b09111d883c5
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2025-02-08 12:07:12 -0600

    Initial Product Edit Form Complete.

--- group-13-project/src/app/products/[product_id]/edit/page.tsx
+++ group-13-project/src/app/products/[product_id]/edit/page.tsx
@@ -10,34 +10,14 @@ export default async function Page(props: {params: Promise<{product_id: string}>
         const [product] = await Promise.all([
             fetchProductById(product_id),
         ]);
-        console.log(product.category);
-        // TODO
-        // Edit form html / css / stuff
-        // Figure out states to get userId?
-        // Validate database info get
-        // Fill in via database get
-        // validate form data
-        // push to server
-        // server side checks
-        // push to database
-
-        // ;-; I forgot how complicated this got
 
         return (
             <main>
-                <p>{product.product_id}</p>
-                <p>{product.product_name}</p>
-                <p>{product.price_in_cents}</p>
-                <p>{product.category}</p>
-                <p>{product.description}</p>
-                <p>{product.created_at.toString()}</p>
                 <EditProductForm product={product}/>
             </main>
         );
 
     } catch (error) {
-        // Should check for specific errors and pass not found page info
-        console.log(error);
         notFound();
     }
 }
