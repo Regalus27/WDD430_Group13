b9abc1b453cf6c7376e69e06a920953e75584475
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2025-02-08 15:45:19 -0600

    Modified Metadata on Edit Product Page

--- group-13-project/src/app/products/[product_id]/edit/page.tsx
+++ group-13-project/src/app/products/[product_id]/edit/page.tsx
@@ -1,6 +1,11 @@
 import EditProductForm from "@/app/ui/editProductForm";
 import { fetchProductById } from "@/lib/data";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+
+export const metadata: Metadata = {
+    title: 'Edit Product',
+};
 
 export default async function Page(props: {params: Promise<{product_id: string}>}) {
     const params = await props.params;
