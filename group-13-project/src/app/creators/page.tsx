c1a51e15fd1798fcca6441be8a6098a4b79f003b
divineProjects <divinejigu@gmail.com>
2025-02-14 00:20:08 +0200

    pagination

--- group-13-project/src/app/creators/page.tsx
+++ group-13-project/src/app/creators/page.tsx
@@ -3,8 +3,18 @@ import {fetchUserProfiles} from '@/lib/data';
 
 import Image from 'next/image';
 import { BtnViewArtist, BtnViewArtistDesktop } from '@/app/ui/creators/buttons';
+import Search from "@/app/ui/search"; 
+import CreatorsTable from '@/app/ui/creators'
 
-export default async function Page() {
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
     const users = await fetchUserProfiles();
   return (
@@ -15,75 +25,13 @@ export default async function Page() {
       </h2>
       <div className="mt-6 flow-root">
         <div className="inline-block min-w-full align-middle">
-          <div className="rounded-3xl bg-white/90 p-6 shadow-xl backdrop-blur-md">
-            {/* Mobile List View */}
-            <div className="md:hidden">
-              {users?.map((user) => (
-                <div
-                  key={user.user_id}
-                  className="mb-4 w-full rounded-lg bg-white p-6 shadow-lg transition hover:scale-105 duration-300"
-                >
-                  <div className="flex items-center justify-between border-b pb-4">
-                    <div className="flex items-center gap-3">
-                      <Image
-                        src={"/mockup.png"}
-                        className="rounded-full border-4 border-yellow-400 shadow-md"
-                        width={60}
-                        height={60}
-                        alt={`${user.name}'s profile`}
-                      />
-                      <p className="text-lg font-semibold text-gray-900">
-                        {user.name}
-                      </p>
-                    </div>
-                    <BtnViewArtist id={user.user_id} />
-                  </div>
-                  <div className="flex w-full justify-between pt-4">
-                    <p className="text-purple-600 font-medium">{user.artstyle}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-
-            {/* Desktop Table View */}
-            <table className="hidden min-w-full text-gray-900 md:table">
-              <thead className="rounded-lg bg-indigo-700 text-white text-sm font-semibold">
-                <tr>
-                  <th scope="col" className="px-4 py-5 sm:pl-6 text-lg ">
-                    Creator
-                  </th>
-                  <th scope="col" className="px-3 py-5 text-lg text-left"> Style</th>
-                  <th scope="col" className="relative py-3 pl-6 pr-3">
-                    <span className="sr-only">View</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white">
-                {users?.map((user) => (
-                  <tr
-                    key={user.user_id}
-                    className="border-b py-3 text-md last:border-none hover:bg-gray-100 transition duration-300"
-                  >
-                    <td className="whitespace-nowrap py-4 pl-6 pr-3 flex items-center gap-4">
-                      <Image
-                        src={"/mockup.png"}
-                        className="rounded-full border-2 border-pink-500 shadow-md"
-                        width={60}
-                        height={60}
-                        alt={`${user.name}'s profile`}
-                      />
-                      <p className="text-gray-900 font-semibold">{user.name}</p>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-purple-700 font-medium">
-                      {user.artstyle}
-                    </td>
-                    <td className="whitespace-nowrap py-3 pl-6 pr-3 flex justify-end">
-                      <BtnViewArtistDesktop id={user.user_id} />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+            <Search placeholder="Search creators ..." />
+            {/* <CreateInvoice /> */}
+          </div>
+          kkkkkkkkkk
+          <div className="mt-5 flex w-full justify-center">
+            {/* <Pagination totalPages={totalPages} /> */}
           </div>
         </div>
       </div>
