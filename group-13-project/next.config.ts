49ea504b87e7739859264393a1110707b8616358
jegs01 <bodcafe@gmail.com>
2025-02-19 12:16:29 +0100

    Rectified Cart Removal Bug

--- group-13-project/next.config.ts
+++ group-13-project/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["057mycosqxcgnfo1.public.blob.vercel-storage.com"],
+  },
 };
 
 export default nextConfig;
