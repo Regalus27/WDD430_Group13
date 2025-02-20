760ac81c4d6279161837e78afbb462ec405c6408
divineProjects <divinejigu@gmail.com>
2025-02-20 22:17:43 +0200

    changed headings for /creator/id from product to curated collection

--- group-13-project/src/app/creators/[id]/page.tsx
+++ group-13-project/src/app/creators/[id]/page.tsx
@@ -81,7 +81,7 @@ export default async function Page(props: {params: Promise<{id: string}>}) {
         
         {/* Creator Products */}
         <div className="mt-6">
-          <h3 className="text-lg font-semibold text-gray-900"> Creator Product: </h3>
+          <h3 className="text-lg font-semibold text-gray-900"> Curated Collection: </h3>
         
           <Suspense>
               <ProductGallery products ={products} />
